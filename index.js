283264cc763e3555e13e81bacfd3369aa8efc367
João Felipe Lima de Oliveira <joaofelipe.work@gmail.com>
2022-08-03 15:46:42 -0300

    For analysis

--- index.js
+++ index.js
@@ -3,17 +3,13 @@
 if (document.readyState == "loading") document.addEventListener('DOMContentLoaded', init)
 else init()
 
-var isPanelAdded = false
-
 function init() {
     const content = document.querySelectorAll(".WidgetSimple .Header")
 
     var isIcAdded = icVerifier(content)
     var isCorrectPage = pageVerifier()
-    var isIcPage = icPageVerifier()
 
     if (isCorrectPage && !isIcAdded) addAlert()
-    if (isIcPage) setTimeout(addIc, 1000)
 }
 
 function getTicket() {
@@ -30,32 +26,21 @@ function getTicket() {
     }
 }
 
-function getChallengeToken() {
-    try {
-        const logoutButton = document.getElementsByClassName('LogoutButton')[0]
-        const challengeToken = logoutButton.href.toString().split('ChallengeToken=')[1]
-        return challengeToken.replace(';','')
-    }  catch (error) {
-        console.log(error);
-    }
-}
-
 function addAlert() {
     const body = document.getElementsByTagName('body')[0]
-    const head = document.getElementsByTagName('head')[0]
+    var ticket = getTicket()
 
     body.insertAdjacentHTML('afterbegin', 
     `
-    <div class="alert">
-        <h1 class="icon">!</h1>
-        <span class="span">
-            IC não associado
-        </span>
-    </div>
+    <a class="alert" title="Clique para adicionar o IC" href="https://suportedti.agu.gov.br/otrs/index.pl?Action=AgentLinkObject;SourceObject=Ticket;SourceKey=${ticket}" target="_blanck" alt=">
+        <div class="alert">
+            <h1 class="icon">!</h1>
+            <span class="span">
+                IC não associado
+            </span>
+        </div>
+    </a>
     `)
-    
-    const alert = document.getElementsByClassName('alert')[0]
-    alert.addEventListener('click', injectPanel)
 }
 
 function pageVerifier() {
@@ -77,69 +62,4 @@ function icVerifier(content) {
         }  catch {}
     }
     return isIcAdded
-}
-
-function icPageVerifier() {
-    try {
-        return document.getElementsByTagName('h1')[0].textContent.includes('Gerenciar links para Chamado#')
-    }   catch (error) {
-        console.log(error);
-    }
-}
-
-function addIc() {
-    if (document.getElementsByClassName('MessageBox Info').length == 0){
-        document.getElementById('LinkTargetKeys').click()
-        document.getElementById('AddLinks').click()
-
-        const body = document.getElementsByTagName('body')[0]
-        body.insertAdjacentHTML('afterbegin', 
-        `
-        <div class="loading">
-            <span class="loader"></span>
-        </div>
-        `)
-    } else {
-        window.close()
-    }
-}
-
-function injectPanel() {
-    if (!isPanelAdded) {
-        isPanelAdded = true
-        var challengeToken = getChallengeToken().toString()
-        var ticket = getTicket()
-        console.log(challengeToken);
-        console.log(ticket);
-
-        document.getElementsByClassName('alert')[0].classList.add('icPanelAdded')
-        document.getElementsByClassName('span')[0].classList.add('turnSpanVisible')
-        document.getElementsByClassName('icon')[0].classList.add('moveIcon')
-
-        var teste = 'asdfdf'
-
-        const body = document.getElementsByTagName('body')[0]
-        body.insertAdjacentHTML('afterbegin', 
-        `
-        <div class="icPanel">
-            <div class="header">
-                <h2 class="title">Adicionar IC</h2>
-            </div>
-            <div class="options">
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?ChallengeToken=${challengeToken}&Action=AgentLinkObject&Mode=Normal&SourceObject=Ticket&SourceKey=${ticket}&TargetIdentifier=ITSMConfigItem%3A%3A139&SEARCH%3A%3ANumber=&SEARCH%3A%3AName=biblioteca&SubmitSearch=Procurar&TypeIdentifier=AlternativeTo%3A%3ASource&SubmitLink=Procurar" target="_blanck">Bib. Software</a></button>
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?ChallengeToken=${challengeToken}&Action=AgentLinkObject&Mode=Normal&SourceObject=Ticket&SourceKey=${ticket}&TargetIdentifier=ITSMConfigItem%3A%3A168&SEARCH%3A%3ANumber=&SEARCH%3A%3AName=SDFURA0031&SubmitSearch=Procurar&TypeIdentifier=AlternativeTo%3A%3ASource&SubmitLink=Procurar" target="_blanck">Reset de senha</a></button>
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?ChallengeToken=${challengeToken}&Action=AgentLinkObject&Mode=Normal&SourceObject=Ticket&SourceKey=${ticket}&TargetIdentifier=ITSMConfigItem%3A%3A168&SEARCH%3A%3ANumber=&SEARCH%3A%3AName=SDF0432&SubmitSearch=Procurar&TypeIdentifier=AlternativeTo%3A%3ASource&SubmitLink=Procurar" target="_blanck">Alt. na conta</a></button>
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?ChallengeToken=${challengeToken}&Action=AgentLinkObject&Mode=Normal&SourceObject=Ticket&SourceKey=${ticket}&TargetIdentifier=ITSMConfigItem%3A%3A168&SEARCH%3A%3ANumber=&SEARCH%3A%3AName=SDF0814+&SubmitSearch=Procurar&TypeIdentifier=AlternativeTo%3A%3ASource&SubmitLink=Procurar" target="_blanck">Desativar conta</a></button>
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?ChallengeToken=${challengeToken}&Action=AgentLinkObject&Mode=Normal&SourceObject=Ticket&SourceKey=${ticket}&TargetIdentifier=ITSMConfigItem%3A%3A168&SEARCH%3A%3ANumber=&SEARCH%3A%3AName=SDFURA0117&SubmitSearch=Procurar&TypeIdentifier=AlternativeTo%3A%3ASource&SubmitLink=Procurar" target="_blanck">Criar conta</a></button>
-                <button class="button"><a class="linkButton" href="https://suportedti.agu.gov.br/otrs/index.pl?Action=AgentLinkObject;SourceObject=Ticket;SourceKey=${ticket}" target="_blanck">Inserir manual</a></button>
-            </div>
-        </div>
-        `)
-    } else {
-        isPanelAdded = false
-        document.getElementsByClassName('alert')[0].classList.remove('icPanelAdded')
-        document.getElementsByClassName('span')[0].classList.remove('turnSpanVisible')
-        document.getElementsByClassName('icon')[0].classList.remove('moveIcon')
-        document.getElementsByClassName('icPanel')[0].remove()
-    }
 }
