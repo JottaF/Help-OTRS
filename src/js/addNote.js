e6b017e6e0500b50a061bf8b819967b99ec66028
Joao Felipe Lima De Oliveira <hepta.joao.o@AGU.GOV.BR>
2023-12-13 15:12:00 -0300

    feat: Added close button

--- src/js/addNote.js
+++ src/js/addNote.js
@@ -9,14 +9,14 @@ export function addNote() {
 
                     document.querySelector('#Subject').setAttribute('value', 'Em atendimento')
                     document.querySelector('#Subject').textContent = 'Em atendimento'
-            
+
                     const iframe = document.querySelector('#cke_RichText').querySelector('iframe')
                     const doc = iframe.contentDocument.querySelector('.cke_editable.cke_editable_themed.cke_contents_ltr.cke_show_borders')
                     doc.textContent = 'Em atendimento'
-            
+
                     document.querySelector('#submitRichText').click()
                 }
-            })  
+            })
         }, 1500);
     });
 }
@@ -24,28 +24,30 @@ export function addNote() {
 function trEventlistener(table) {
     try {
         const contextMenu = document.getElementById('context-menu')
+        const closeButton = document.getElementById('close-menu-button')
 
         Array.from(table.getElementsByTagName('tr')).forEach((element) => {
-            function showRDMInfoAndCloseContextMenu() {
-                contextMenu.style.display = 'none'
-                document.querySelector('#link-chamado').removeAttribute('href')
-            }
-            
             element.addEventListener('contextmenu', e => {
                 e.preventDefault()
-                
-                document.querySelector('#load-page').onclick = showRDMInfoAndCloseContextMenu
 
-                const url = element.querySelector('.AsBlock.MasterActionLink').href.replace('Zoom', 'Note')  + ';AddNoteAuto'
+                const url = element.querySelector('.AsBlock.MasterActionLink').href.replace('Zoom', 'Note') + ';AddNoteAuto'
                 document.querySelector('#link-chamado').setAttribute('href', url)
 
                 contextMenu.style.left = `${e.clientX}px`
                 contextMenu.style.top = `${e.clientY + window.scrollY}px`
                 contextMenu.style.display = 'block'
 
-                document.addEventListener('click', hideContextMenu)
-            })
 
+                closeButton.addEventListener('click', hideContextMenu)
+                document.addEventListener('click', hideContextMenu)
+
+                //Fecha o context-menu quando aperta esc
+                document.addEventListener('keydown', event => {
+                    if (event.key === 'Escape') {
+                        hideContextMenu()
+                    }
+                })
+            })
             function hideContextMenu() {
                 contextMenu.style.display = 'none'
                 document.removeEventListener('click', hideContextMenu)
@@ -62,16 +64,16 @@ export function tableObserver() {
         try {
             trEventlistener(table)
             addVerifyAllLink(table)
-    
+
             // Cria uma nova instância do MutationObserver
             const observer = new MutationObserver(() => {
                 trEventlistener(table)
                 addVerifyAllLink(table)
             })
-    
+
             // Define as opções do MutationObserver
             const config = { attributes: true, childList: true, subtree: true }
-    
+
             // Inicia a observação do elemento HTML com as opções definidas
             observer.observe(table, config)
         } catch (error) {
@@ -83,6 +85,9 @@ export function tableObserver() {
 export function injectContextMenu() {
     const html = `
     <div id="context-menu">
+        <div id="close-menu-button">
+            <span>x</span>
+        </div>
         <ul id="menu-list">
             <li id="load-page"><a id="link-chamado" target="_blank">Em atendimento</a></li>
         </ul>
